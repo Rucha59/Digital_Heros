28e5c6d1b90eef36ab7008861e0244bb60973786
Rucha59 <ruchasp9@gmail.com>
2026-07-25 23:59:05 +0530

    Updated backend api call in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': 'https://digital-heros-mmyr.onrender.com',
     },
   },
 })
